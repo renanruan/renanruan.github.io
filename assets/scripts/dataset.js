4444a7bac8f74eff704a0d5318453e1062920efc
Renan Ruan <renan.rrfs@gmail.com>
2024-07-26 07:22:38 -0300

    Add projects info

---
+++ assets/scripts/dataset.js
@@ -0,0 +1,162 @@
+window.dataset = [
+    {
+        id: 0,
+        highlight: 5, 
+        category: ["traffic"],
+        title:"Control and Operation System for Road Radars", 
+        date:"Aug 2022", 
+        description:"<p>This project is a desktop application for Windows designed to interface with highway radar systems. It was one of my first ventures into working with third-party devices, specifically radar systems, and was instrumental in deepening my understanding of industry practices, collaboration with other companies, and integration of external systems.</p><p>The application features a graphical user interface and supports communication via TCP, UDP, and serial protocols with local radar systems. It enables users to configure radar settings (such as speed limits and sampling intervals), monitor activities (including logs of recent vehicles and their speeds), and record data in text format. Additionally, it supports network configuration to ensure smooth operation.</p><p>For this project, I employed C# for programming, Visual Studio as the integrated development environment, and Windows Forms (WForms) for the user interface. The use of communication protocols like TCP, UDP, and serial facilitated reliable data transmission and effective interaction with radar systems.</p>", 
+        tag: ["C#", "TCP/IP", "Serial COM", "Windows Forms", "Visual Studio"],
+        images: ["assets/imgs/Project_Radar/Pic1.png","assets/imgs/Project_Radar/Pic2.png","assets/imgs/Project_Radar/Pic3.png"]
+    },
+    {
+        id: 1,
+        highlight: -40, 
+        category: ["traffic"],
+        title:"Control and Operation System for Road Tolls", 
+        date:"2020", 
+        description:"<p></p>", 
+        tag: ["C#", "TCP/IP", "Serial COM", "Windows Forms","Visual Studio", "Arduino"],
+        images: [""]
+    },
+    {
+        id: 2,
+        highlight: 12, 
+        category: ["games", "mobile"],
+        title:"Listen & Catch Up", 
+        date:"Aug 2018", 
+        description:"<p>In 2018, I collaborated with the Center for Game and Internet Studies (CEGI), an extension group of UNIFESP, to develop an educational game aimed at teaching English to children. As a programmer, I worked closely with a multidisciplinary team of students to bring this project to life. The game is open-source and available for free on <a href=''>GitHub</a>.</p><p>The development process utilized several key technologies. C# was employed for its powerful and efficient programming capabilities, while Unity3D provided a versatile platform for creating engaging game environments. Visual Studio served as our integrated development environment, streamlining the coding, debugging, and testing processes. This combination of technologies ensured the creation of an effective and accessible educational tool.</p>", 
+        tag: ["C#", "Unity3D", "Visual Studio"],
+        images: ["assets/imgs/Project_Listen&CatchUp/Pic1.png","assets/imgs/Project_Listen&CatchUp/Pic2.png","assets/imgs/Project_Listen&CatchUp/Pic3.png","assets/imgs/Project_Listen&CatchUp/Pic4.png"]
+    },
+    {
+        id: 3,
+        highlight: -16, 
+        category: ["aerospace"],
+        title:"Thermocouple Monitoring System", 
+        date:"2015", 
+        description:"<p></p>", 
+        tag: ["C#", "Serial COM", "Windows Forms","Visual Studio", "Arduino", "C"],
+        images: [""]
+    },
+    {
+        id: 4,
+        highlight: -15, 
+        category: ["aerospace"],
+        title:"Monitoring System for Unmanned Aerial Vehicles (UAVs)", 
+        date:"2017", 
+        description:"<p></p>", 
+        tag: ["C#", "Serial COM", "Google API", "Windows Forms","Visual Studio", "Arduino", "C"],
+        images: [""]
+    },
+    {
+        id: 5,
+        highlight: -20, 
+        category: ["aerospace"],
+        title:"Project Risk Reporting Software", 
+        date:"2017", 
+        description:"<p></p>", 
+        tag: ["C#", "Windows Forms","Visual Studio"],
+        images: [""]
+    },
+    {
+        id: 6,
+        highlight: 4, 
+        category: ["mobile"],
+        title:"Inventory System for Windows", 
+        date:"Mar 2022", 
+        description:"<p>I developed a comprehensive control system for stores, featuring functionalities such as account creation, user logins, inventory registration, and management. This project is a Windows desktop application with a user-friendly graphical interface. It integrates seamlessly with an online server and utilizes free data storage on Firebase.</p><p>In this project, I used C# for its robustness and efficiency in developing desktop applications. Visual Studio provided a powerful integrated development environment for coding, debugging, and testing. Windows Forms (WForms) was employed to create an intuitive and responsive graphical interface. The system's backend leverages ASP.NET to handle server-side operations, while FireStore (Firebase) offers reliable and free data storage solutions. This combination of technologies ensured the development of a reliable, scalable, and user-friendly control system.</p>", 
+        tag: ["C#", "ASP .NET", "FireStore", "REST API", "Visual Studio"],
+        images: ['assets/imgs/Project_Inventory/Pic1.png','assets/imgs/Project_Inventory/Pic2.png','assets/imgs/Project_Inventory/Pic3.png','assets/imgs/Project_Inventory/Pic4.png','assets/imgs/Project_Inventory/Pic5.png','assets/imgs/Project_Inventory/Pic6.png','assets/imgs/Project_Inventory/Pic7.png']
+    },
+    {
+        id: 7,
+        highlight: 6, 
+        category: ["games"],
+        title:"Don\'t Lose Your Head", 
+        date:"Jan 2021", 
+        description:"<p>Don\'t Lose Your Head is a dynamic Bullet Hell game developed in 2021 for the international Global Game Jam. The theme for that year was Lost & Found and I collaborated with a talented 2D artist and a sound designer to create this game in just 4 days using Unity3D.</p><p>The game is highly energetic and can be played <a href=''>here</a> for free on any desktop via a web browser. Check out the game trailer on <a href=''>YouTube</a>!</p><p>During the development we used Unity3D for its versatile platform, C# for robust programming, Visual Studio for a streamlined workflow and FMOD for immersive sound design. This combination ensured a polished and engaging gaming experience.</p>", 
+        tag: ["C#", "Unity3D", "Visual Studio", "FMOD"],
+        images: ["assets/imgs/Project_DontLoseYourHead/Pic1.png","assets/imgs/Project_DontLoseYourHead/Pic2.png","assets/imgs/Project_DontLoseYourHead/Pic3.png","assets/imgs/Project_DontLoseYourHead/Pic4.png"]
+    },
+    {
+        id: 8,
+        highlight: -10, 
+        category: ["games"],
+        title:"MKS Unity Challenge", 
+        date:"Feb 2023", 
+        description:"<p>In 2023, MKS hosted a challenge for Unity3D developers to showcase their skills, providing a set of assets and project guidelines. I created 'MKS Unity Challenge' a fun and engaging game where players control a boat and must defeat pirates within a limited timeframe. The game was developed entirely in Unity3D and is available to play for free <a href=''>here</a> on any desktop browser.</p><p>The project utilized Unity3D for its powerful game development capabilities, C# for programming, and Visual Studio as the integrated development environment. This combination of technologies allowed me to develop a dynamic and enjoyable gaming experience.</p>", 
+        tag: ["C#", "Unity3D", "Visual Studio"],
+        images: ["assets/imgs/Project_MKSChallenge/Pic1.png","assets/imgs/Project_MKSChallenge/Pic2.png","assets/imgs/Project_MKSChallenge/Pic3.png","assets/imgs/Project_MKSChallenge/Pic4.png","assets/imgs/Project_MKSChallenge/Pic5.png","assets/imgs/Project_MKSChallenge/Pic6.png"]
+    },
+    {
+        id: 9,
+        highlight: -25, 
+        category: ["games"],
+        title:"Little Goats Quiz", 
+        date:"May 2019", 
+        description: "<p>In 2019, I collaborated with the Center for Game and Internet Studies (CEGI), an extension group of UNIFESP, to develop an educational game for elementary schools in São José dos Campos.</p><p>The game's objective is to enhance children's reading comprehension by combining a children's story with an interactive quiz. Additionally, it features videos in Brazilian Sign Language (Libras) to ensure greater accessibility.</p><p>As a programmer on this project, I worked closely with a multidisciplinary team of students, utilizing my skills in C#, Unity3D, and Visual Studio to bring the game to life. The game is open-source and available for free on <a href=''>GitHub</a>. This experience not only allowed me to apply my technical expertise but also to contribute to a meaningful educational initiative.</p>", 
+        tag: ["C#", "Unity3D", "Visual Studio"],
+        images: ["assets/imgs/Project_GoatQuiz/Pic1.png","assets/imgs/Project_GoatQuiz/Pic2.png","assets/imgs/Project_GoatQuiz/Pic3.png","assets/imgs/Project_GoatQuiz/Pic4.png"]
+    },
+    {
+        id: 10,
+        highlight: -9, 
+        category: ["games"],
+        title:"Scribble Scrabble", 
+        date:"Feb 2021", 
+        description:"<p></p>", 
+        tag: ["C#", "Unity3D", "Visual Studio"],
+        images: [""]
+    },
+    {
+        id: 11,
+        highlight: 3, 
+        category: ["games"],
+        title:"Time Zooka", 
+        date:"Jul 2021", 
+        description:"<p></p>", 
+        tag: ["C#", "Unity3D", "Visual Studio"],
+        images: [""]
+    },
+    {
+        id: 12,
+        highlight: -10, 
+        category: ["traffic"],
+        title:"Internal Traffic Surveillance System", 
+        date:"Aug 2023", 
+        description:"<p></p>", 
+        tag: ["C#", "TCP/IP", "Pulmatronix Camera", "Windows Forms", "Visual Studio", "Serial COM"],
+        images: [""]
+    },
+    {
+        id: 13,
+        highlight: 2, 
+        category: ["traffic"],
+        title:"Internal Traffic Surveillance System with Speed Control", 
+        date:"Jun 2024", 
+        description:"<p></p>", 
+        tag: ["C#", "REST API", "Pulmatronix Camera", "TCP/IP","Windows Forms", "Visual Studio", "Serial COM"],
+        images: [""]
+    },
+    {
+        id: 14,
+        highlight: 1, 
+        category: ["ai","aerospace"],
+        title:"Machine Learning as a Tool for Enhancing Air Navigation in Low Latitudes", 
+        date:"Jun 2024", 
+        description:"<p></p>", 
+        tag: ["Python", "Google Colab", "Keras", "PyThorch"],
+        images: [""]
+    },
+    {
+        id: 15,
+        highlight: -35, 
+        category: ["mobile","web"],
+        title:"Portifolio Web Site", 
+        date:"Jul 2024", 
+        description:"<p></p>", 
+        tag: ["HTML", "CSS", "JavaScript"],
+        images: [""]
+    }
+];
--- assets/scripts/projects_summary.js
+++ assets/scripts/projects_summary.js
@@ -1,125 +1,7 @@
-const dataset = [
-    {
-        highlight: 5, 
-        category: ["traffic"],
-        title:"Control and Operation System for Road Radars", 
-        date:"2021", 
-        description:"<p></p>", 
-        tag: ["C#", "TCP/IP", "Serial COM", "Windows Forms", "Visual Studio"]
-    },
-    {
-        highlight: -40, 
-        category: ["traffic"],
-        title:"Control and Operation System for Road Tolls", 
-        date:"2020", 
-        description:"<p></p>", 
-        tag: ["C#", "TCP/IP", "Serial COM", "Windows Forms","Visual Studio", "Arduino"]
-    },
-    {
-        highlight: 12, 
-        category: ["games", "mobile"],
-        title:"Listen & Catch Up", 
-        date:"2018", 
-        description:"<p></p>", 
-        tag: ["C#", "Unity3D", "Visual Studio"]
-    },
-    {
-        highlight: -16, 
-        category: ["aerospace"],
-        title:"Thermocouple Monitoring System", 
-        date:"2015", 
-        description:"<p></p>", 
-        tag: ["C#", "Serial COM", "Windows Forms","Visual Studio", "Arduino", "C"]
-    },
-    {
-        highlight: -15, 
-        category: ["aerospace"],
-        title:"Monitoring System for Unmanned Aerial Vehicles (UAVs)", 
-        date:"2017", 
-        description:"<p></p>", 
-        tag: ["C#", "Serial COM", "Google API", "Windows Forms","Visual Studio", "Arduino", "C"]
-    },
-    {
-        highlight: -20, 
-        category: ["aerospace"],
-        title:"Project Risk Reporting Software", 
-        date:"2017", 
-        description:"<p></p>", 
-        tag: ["C#", "Windows Forms","Visual Studio"]
-    },
-    {
-        highlight: 4, 
-        category: ["mobile"],
-        title:"Inventory System for Windows", 
-        date:"2023", 
-        description:"<p></p>", 
-        tag: ["C#", "ASP .NET", "FireStore", "REST API", "Visual Studio"]
-    },
-    {
-        highlight: 6, 
-        category: ["games"],
-        title:"Don't Lose Your Head", 
-        date:"Jan 2021", 
-        description:"<p></p>", 
-        tag: ["C#", "Unity3D", "Visual Studio", "FMOD"]
-    },
-    {
-        highlight: -10, 
-        category: ["games"],
-        title:"MKS Unity Challenge", 
-        date:"Feb 2023", 
-        description:"<p></p>", 
-        tag: ["C#", "Unity3D", "Visual Studio"]
-    },
-    {
-        highlight: -25, 
-        category: ["games"],
-        title:"Little Goats Quiz", 
-        date:"2019", 
-        description:"<p></p>", 
-        tag: ["C#", "Unity3D", "Visual Studio"]
-    },
-    {
-        highlight: -9, 
-        category: ["games"],
-        title:"Scribble Scrabble", 
-        date:"Feb 2021", 
-        description:"<p></p>", 
-        tag: ["C#", "Unity3D", "Visual Studio"]
-    },
-    {
-        highlight: 3, 
-        category: ["games"],
-        title:"Time Zooka", 
-        date:"Jul 2021", 
-        description:"<p></p>", 
-        tag: ["C#", "Unity3D", "Visual Studio"]
-    },
-    {
-        highlight: -10, 
-        category: ["traffic"],
-        title:"Internal Traffic Surveillance System", 
-        date:"Aug 2023", 
-        description:"<p></p>", 
-        tag: ["C#", "TCP/IP", "Pulmatronix Camera", "Windows Forms", "Visual Studio", "Serial COM"]
-    },
-    {
-        highlight: 2, 
-        category: ["traffic"],
-        title:"Internal Traffic Surveillance System with Speed Control", 
-        date:"Jun 2024", 
-        description:"<p></p>", 
-        tag: ["C#", "REST API", "Pulmatronix Camera", "TCP/IP","Windows Forms", "Visual Studio", "Serial COM"]
-    },
-    {
-        highlight: 1, 
-        category: ["ai","aerospace"],
-        title:"Machine Learning as a Tool for Enhancing Air Navigation in Low Latitudes", 
-        date:"Jun 2024", 
-        description:"<p></p>", 
-        tag: ["Python", "Google Colab", "Keras", "PyThorch"]
-    }
-];
+const dataset = window.dataset;
+
+var current_slide = 0;
+var current_project;
 
 function generateFilterContent(category){
     const filteredData = dataset.filter(item => item.category.includes(category));
@@ -154,7 +36,7 @@ function fillSampleContainer(elementsList){
         html += `
                 <div class="project-sample">
                 <div class="sample-image">
-
+                    <img src="${item.images[0]}"></img>
                 </div>
                     <div class="sample-description">
                         <div class="sample-text">
@@ -163,7 +45,7 @@ function fillSampleContainer(elementsList){
                             ${item.description}
                         </div>
                         <div class="sample-bar">
-                            <button onclick="showProject('${item.title}')">${readButton}</button>
+                            <button onclick="showProject('${item.id}')">${readButton}</button>
         `;
         var max_tags = 3;
         item.tag.forEach(function(tagling){
@@ -230,28 +112,26 @@ function redirectToProjects()
     window.location.href = 'projects_summary.html';
 }
 
-function showProject(project_title){
+function showProject(project_id){
 
-    console.log("mudor de pagina");
+    current_slide = 0
 
-    const project = dataset.find(element => element.title === project_title);
+    current_project = dataset.find(element => element.id == project_id);
     const container = document.getElementById("project-container");
     container.innerHTML = '';
 
     if(container){
-        console.log("achou");
         var html = `
         <div class="project-content">
             <div class="project-text">
-                <h2>${project.title}</h2>
-                <h3>${project.date}</h3>
-                ${project.description}
+                <h2>${current_project.title}</h2>
+                <h3>${current_project.date}</h3>
+                ${current_project.description}
             </div>
             <div class="project-tags">
-                <span>TAG</span>
         `;
 
-        project.tag.forEach(function(tagling){
+        current_project.tag.forEach(function(tagling){
             const tag_html = `<span>${tagling}</span>`;
             html += tag_html;
         });
@@ -259,10 +139,12 @@ function showProject(project_title){
 
         html += `
             </div>
-            <div class="project-images">
-                <img></img>
-                <button class="btn left">←</button>
-                <button class="btn right">→</button>
+            <div id="image_frame" class="project-images">
+                <img src="${current_project.images[current_slide]}" onclick="showImageSrc('${current_project.images[current_slide]}')"></img>
+            </div>
+            <div class="slide-buttons">
+                <button class="btn left" onclick="slideImage(-1)">PREV</button>
+                <button class="btn right" onclick="slideImage(1)">NEXT</button>
             </div>
         </div>
         <div class="filler"></div>
@@ -276,4 +158,24 @@ function showProject(project_title){
     }
 
     
+}
+
+function slideImage(offset)
+{
+    current_slide = (offset + current_slide) % current_project.images.length;
+
+    if(current_slide < 0){
+        current_slide = current_project.images.length - 1;
+    }
+
+    const image_frame = document.getElementById('image_frame');
+
+    if(image_frame){
+        image_frame.innerHTML = `<img src="${current_project.images[current_slide]}" onclick="showImageSrc('${current_project.images[current_slide]}')"></img>`
+    }
+}
+
+function showImageSrc(img)
+{
+    window.open(img,'_blank');
 }
