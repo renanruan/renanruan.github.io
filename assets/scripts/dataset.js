eb490b404968b0fda83177549bbe9bf73b44bac0
Renan Ruan <renan.rrfs@gmail.com>
2024-07-26 17:37:48 -0300

    More projects info

--- assets/scripts/dataset.js
+++ assets/scripts/dataset.js
@@ -5,7 +5,7 @@ window.dataset = [
         category: ["traffic"],
         title:"Control and Operation System for Road Radars", 
         date:"Aug 2022", 
-        description:"<p>This project is a desktop application for Windows designed to interface with highway radar systems. It was one of my first ventures into working with third-party devices, specifically radar systems, and was instrumental in deepening my understanding of industry practices, collaboration with other companies, and integration of external systems.</p><p>The application features a graphical user interface and supports communication via TCP, UDP, and serial protocols with local radar systems. It enables users to configure radar settings (such as speed limits and sampling intervals), monitor activities (including logs of recent vehicles and their speeds), and record data in text format. Additionally, it supports network configuration to ensure smooth operation.</p><p>For this project, I employed C# for programming, Visual Studio as the integrated development environment, and Windows Forms (WForms) for the user interface. The use of communication protocols like TCP, UDP, and serial facilitated reliable data transmission and effective interaction with radar systems.</p>", 
+        description:"<p>This project is a desktop application for Windows designed to interface with highway radar systems. It was one of my first ventures into working with third-party devices, specifically radar systems and was instrumental in deepening my understanding of industry practices, collaboration with other companies and integration of external systems.</p><p>The application features a graphical user interface and supports communication via TCP, UDP and serial protocols with local radar systems. It enables users to configure radar settings (such as speed limits and sampling intervals), monitor activities (including logs of recent vehicles and their speeds) and record data in text format. Additionally, it supports network configuration to ensure smooth operation.</p><p>For this project, I employed C# for programming, Visual Studio as the integrated development environment and Windows Forms (WForms) for the user interface. The use of communication protocols like TCP, UDP and serial facilitated reliable data transmission and effective interaction with radar systems.</p>", 
         tag: ["C#", "TCP/IP", "Serial COM", "Windows Forms", "Visual Studio"],
         images: ["assets/imgs/Project_Radar/Pic1.png","assets/imgs/Project_Radar/Pic2.png","assets/imgs/Project_Radar/Pic3.png"]
     },
@@ -14,10 +14,10 @@ window.dataset = [
         highlight: -40, 
         category: ["traffic"],
         title:"Control and Operation System for Road Tolls", 
-        date:"2020", 
-        description:"<p></p>", 
+        date:"Dec 2023", 
+        description:"<p>This freelance project, spanning from 2017 to 2023, focused on integrating toll booths with electronic information signs. It involved developing an embedded system using Arduino alongside a Windows desktop application featuring a graphical interface. The system communicates via TCP, UDP and Serial protocols.</p><p>The application enables users to configure electronic signs (including standard display messages, display time and light intensity), perform automatic tests on panels and lights and set up the network. New versions of the project were released annually to support new models of electronic signs.</p><p>Technologies employed include C# for application development, Visual Studio as the integrated development environment, Windows Forms (WForms) for the graphical interface, communication protocols (TCP, UDP, Serial) for reliable data transmission and C for the embedded Arduino system.</p>", 
         tag: ["C#", "TCP/IP", "Serial COM", "Windows Forms","Visual Studio", "Arduino"],
-        images: [""]
+        images: ["assets/imgs/Project_Tolls/Pic1.png","assets/imgs/Project_Tolls/Pic2.png","assets/imgs/Project_Tolls/Pic3.png","assets/imgs/Project_Tolls/Pic4.png"]
     },
     {
         id: 2,
@@ -25,7 +25,7 @@ window.dataset = [
         category: ["games", "mobile"],
         title:"Listen & Catch Up", 
         date:"Aug 2018", 
-        description:"<p>In 2018, I collaborated with the Center for Game and Internet Studies (CEGI), an extension group of UNIFESP, to develop an educational game aimed at teaching English to children. As a programmer, I worked closely with a multidisciplinary team of students to bring this project to life. The game is open-source and available for free on <a href=''>GitHub</a>.</p><p>The development process utilized several key technologies. C# was employed for its powerful and efficient programming capabilities, while Unity3D provided a versatile platform for creating engaging game environments. Visual Studio served as our integrated development environment, streamlining the coding, debugging, and testing processes. This combination of technologies ensured the creation of an effective and accessible educational tool.</p>", 
+        description:"<p>In 2018, I collaborated with the Center for Game and Internet Studies (CEGI), an extension group of UNIFESP, to develop an educational game aimed at teaching English to children. As a programmer, I worked closely with a multidisciplinary team of students to bring this project to life. The game is open-source and available for free on <a href=''>GitHub</a>.</p><p>The development process utilized several key technologies. C# was employed for its powerful and efficient programming capabilities, while Unity3D provided a versatile platform for creating engaging game environments. Visual Studio served as our integrated development environment, streamlining the coding, debugging and testing processes. This combination of technologies ensured the creation of an effective and accessible educational tool.</p>", 
         tag: ["C#", "Unity3D", "Visual Studio"],
         images: ["assets/imgs/Project_Listen&CatchUp/Pic1.png","assets/imgs/Project_Listen&CatchUp/Pic2.png","assets/imgs/Project_Listen&CatchUp/Pic3.png","assets/imgs/Project_Listen&CatchUp/Pic4.png"]
     },
@@ -34,30 +34,30 @@ window.dataset = [
         highlight: -16, 
         category: ["aerospace"],
         title:"Thermocouple Monitoring System", 
-        date:"2015", 
-        description:"<p></p>", 
+        date:"May 2015", 
+        description:"<p>Developed for testing with temperature sensors (thermocouples), this project consists of a Windows desktop application that connects via Serial communication to an Arduino linked to the thermocouples. The application displays real-time temperature readings through text boxes, time graphs and illustrated images.</p><p>Technologies employed in this project include C# for the application development, Visual Studio as the integrated development environment, Serial communication protocol for interfacing with the Arduino and C for the embedded programming on the Arduino. This project demonstrates my ability to integrate hardware and software to create practical solutions for sensor data visualization and analysis.</p>", 
         tag: ["C#", "Serial COM", "Windows Forms","Visual Studio", "Arduino", "C"],
-        images: [""]
+        images: ["assets/imgs/Project_Thermocouple/Pic1.png","assets/imgs/Project_Thermocouple/Pic2.png","assets/imgs/Project_Thermocouple/Pic3.png"]
     },
     {
         id: 4,
         highlight: -15, 
         category: ["aerospace"],
         title:"Monitoring System for Unmanned Aerial Vehicles (UAVs)", 
-        date:"2017", 
-        description:"<p></p>", 
+        date:"Apr 2015", 
+        description:"<p>Developed to monitor Unmanned Aerial Vehicles (UAVs), this project features a Windows desktop application that interfaces with a VectorNav inertial sensor via Serial communication. The application provides real-time data, including geographic position (GPS), direction, tilt, and velocities. This information is displayed in real time, in both text and graphical formats using Google Maps plugins and can be recorded, saved, and exported.</p><p>Technologies utilized in this project include C# for the application development, Visual Studio as the integrated development environment, Serial communication protocol for sensor interfacing, VectorNav for the inertial sensing and Google Maps for the graphical display of geographic data.</p>", 
         tag: ["C#", "Serial COM", "Google API", "Windows Forms","Visual Studio", "Arduino", "C"],
-        images: [""]
+        images: ["assets/imgs/Project_UAVs/Pic1.png","assets/imgs/Project_UAVs/Pic2.png","assets/imgs/Project_UAVs/Pic3.png"]
     },
     {
         id: 5,
         highlight: -20, 
         category: ["aerospace"],
         title:"Project Risk Reporting Software", 
-        date:"2017", 
-        description:"<p></p>", 
+        date:"Sep 2016", 
+        description:"<p>This is a comprehensive project management system designed for risk assessment and automatic report generation. The Windows desktop application allows users to create projects and input extensive data regarding project safety and potential failures. The software performs calculations to determine risk levels at each stage of the project and enables users to generate PDF reports automatically.</p><p>Developed using C# for programming and Visual Studio as the integrated development environment, the application also includes functionality for generating PDF reports. This streamlines the documentation process and enhances project management efficiency, showcasing my ability to develop robust desktop applications that address critical industry needs.</p>", 
         tag: ["C#", "Windows Forms","Visual Studio"],
-        images: [""]
+        images: ["assets/imgs/Project_RiskReport/Pic1.png","assets/imgs/Project_RiskReport/Pic2.png","assets/imgs/Project_RiskReport/Pic3.png","assets/imgs/Project_RiskReport/Pic4.png","assets/imgs/Project_RiskReport/Pic5.png","assets/imgs/Project_RiskReport/Pic6.png"]
     },
     {
         id: 6,
@@ -65,7 +65,7 @@ window.dataset = [
         category: ["mobile"],
         title:"Inventory System for Windows", 
         date:"Mar 2022", 
-        description:"<p>I developed a comprehensive control system for stores, featuring functionalities such as account creation, user logins, inventory registration, and management. This project is a Windows desktop application with a user-friendly graphical interface. It integrates seamlessly with an online server and utilizes free data storage on Firebase.</p><p>In this project, I used C# for its robustness and efficiency in developing desktop applications. Visual Studio provided a powerful integrated development environment for coding, debugging, and testing. Windows Forms (WForms) was employed to create an intuitive and responsive graphical interface. The system's backend leverages ASP.NET to handle server-side operations, while FireStore (Firebase) offers reliable and free data storage solutions. This combination of technologies ensured the development of a reliable, scalable, and user-friendly control system.</p>", 
+        description:"<p>I developed a comprehensive control system for stores, featuring functionalities such as account creation, user logins, inventory registration and management. This project is a Windows desktop application with a user-friendly graphical interface. It integrates seamlessly with an online server and utilizes free data storage on Firebase.</p><p>In this project, I used C# for its robustness and efficiency in developing desktop applications. Visual Studio provided a powerful integrated development environment for coding, debugging and testing. Windows Forms (WForms) was employed to create an intuitive and responsive graphical interface. The system's backend leverages ASP.NET to handle server-side operations, while FireStore (Firebase) offers reliable and free data storage solutions. This combination of technologies ensured the development of a reliable, scalable and user-friendly control system.</p>", 
         tag: ["C#", "ASP .NET", "FireStore", "REST API", "Visual Studio"],
         images: ['assets/imgs/Project_Inventory/Pic1.png','assets/imgs/Project_Inventory/Pic2.png','assets/imgs/Project_Inventory/Pic3.png','assets/imgs/Project_Inventory/Pic4.png','assets/imgs/Project_Inventory/Pic5.png','assets/imgs/Project_Inventory/Pic6.png','assets/imgs/Project_Inventory/Pic7.png']
     },
@@ -85,7 +85,7 @@ window.dataset = [
         category: ["games"],
         title:"MKS Unity Challenge", 
         date:"Feb 2023", 
-        description:"<p>In 2023, MKS hosted a challenge for Unity3D developers to showcase their skills, providing a set of assets and project guidelines. I created 'MKS Unity Challenge' a fun and engaging game where players control a boat and must defeat pirates within a limited timeframe. The game was developed entirely in Unity3D and is available to play for free <a href=''>here</a> on any desktop browser.</p><p>The project utilized Unity3D for its powerful game development capabilities, C# for programming, and Visual Studio as the integrated development environment. This combination of technologies allowed me to develop a dynamic and enjoyable gaming experience.</p>", 
+        description:"<p>In 2023, MKS hosted a challenge for Unity3D developers to showcase their skills, providing a set of assets and project guidelines. I created 'MKS Unity Challenge' a fun and engaging game where players control a boat and must defeat pirates within a limited timeframe. The game was developed entirely in Unity3D and is available to play for free <a href=''>here</a> on any desktop browser.</p><p>The project utilized Unity3D for its powerful game development capabilities, C# for programming and Visual Studio as the integrated development environment. This combination of technologies allowed me to develop a dynamic and enjoyable gaming experience.</p>", 
         tag: ["C#", "Unity3D", "Visual Studio"],
         images: ["assets/imgs/Project_MKSChallenge/Pic1.png","assets/imgs/Project_MKSChallenge/Pic2.png","assets/imgs/Project_MKSChallenge/Pic3.png","assets/imgs/Project_MKSChallenge/Pic4.png","assets/imgs/Project_MKSChallenge/Pic5.png","assets/imgs/Project_MKSChallenge/Pic6.png"]
     },
@@ -95,7 +95,7 @@ window.dataset = [
         category: ["games"],
         title:"Little Goats Quiz", 
         date:"May 2019", 
-        description: "<p>In 2019, I collaborated with the Center for Game and Internet Studies (CEGI), an extension group of UNIFESP, to develop an educational game for elementary schools in São José dos Campos.</p><p>The game's objective is to enhance children's reading comprehension by combining a children's story with an interactive quiz. Additionally, it features videos in Brazilian Sign Language (Libras) to ensure greater accessibility.</p><p>As a programmer on this project, I worked closely with a multidisciplinary team of students, utilizing my skills in C#, Unity3D, and Visual Studio to bring the game to life. The game is open-source and available for free on <a href=''>GitHub</a>. This experience not only allowed me to apply my technical expertise but also to contribute to a meaningful educational initiative.</p>", 
+        description: "<p>In 2019, I collaborated with the Center for Game and Internet Studies (CEGI), an extension group of UNIFESP, to develop an educational game for elementary schools in São José dos Campos.</p><p>The game's objective is to enhance children's reading comprehension by combining a children's story with an interactive quiz. Additionally, it features videos in Brazilian Sign Language (Libras) to ensure greater accessibility.</p><p>As a programmer on this project, I worked closely with a multidisciplinary team of students, utilizing my skills in C#, Unity3D and Visual Studio to bring the game to life. The game is open-source and available for free on <a href=''>GitHub</a>. This experience not only allowed me to apply my technical expertise but also to contribute to a meaningful educational initiative.</p>", 
         tag: ["C#", "Unity3D", "Visual Studio"],
         images: ["assets/imgs/Project_GoatQuiz/Pic1.png","assets/imgs/Project_GoatQuiz/Pic2.png","assets/imgs/Project_GoatQuiz/Pic3.png","assets/imgs/Project_GoatQuiz/Pic4.png"]
     },
@@ -105,9 +105,9 @@ window.dataset = [
         category: ["games"],
         title:"Scribble Scrabble", 
         date:"Feb 2021", 
-        description:"<p></p>", 
+        description:"<p>Created for the international Scribble Jam competition, Scribble Scrabble is a fun and visually appealing Tower Defense game. Developed in collaboration with a multidisciplinary team over five days, the game embraces the  'Scribbles ' theme and offers an engaging gameplay experience. It is available for free <a href=''>here</a>, on any desktop browser.</p><p>The project utilized Unity3D for game development, C# for programming, and Visual Studio as the integrated development environment. Check out the game trailer on <a href=''>YouTube</a>!</p>", 
         tag: ["C#", "Unity3D", "Visual Studio"],
-        images: [""]
+        images: ["assets/imgs/Project_ScribbleScrabble/Pic1.gif","assets/imgs/Project_ScribbleScrabble/Pic2.png","assets/imgs/Project_ScribbleScrabble/Pic3.png","assets/imgs/Project_ScribbleScrabble/Pic4.png"]
     },
     {
         id: 11,
@@ -115,9 +115,9 @@ window.dataset = [
         category: ["games"],
         title:"Time Zooka", 
         date:"Jul 2021", 
-        description:"<p></p>", 
+        description:"<p>Created for the international competition called A Jam about Time, Time Zooka embraces the 'Time' theme with a unique time manipulation mechanic. Developed in collaboration with a multidisciplinary team over two weeks, this Bullet Hell style game is both dynamic and charming. Available for free <a href=''>here</a>, on any desktop browser, Time Zooka offers an engaging and visually appealing gameplay experience. Check out the game trailer on <a href=''>YouTube</a>!</p><p>The project utilized Unity3D for game development, C# for programming and Visual Studio as the integrated development environment.</p>", 
         tag: ["C#", "Unity3D", "Visual Studio"],
-        images: [""]
+        images: ["assets/imgs/Project_TimeZooka/Pic1.png","assets/imgs/Project_TimeZooka/Pic2.jpg","assets/imgs/Project_TimeZooka/Pic3.jpg","assets/imgs/Project_TimeZooka/Pic4.jpg","assets/imgs/Project_TimeZooka/Pic5.jpg"]
     },
     {
         id: 12,
